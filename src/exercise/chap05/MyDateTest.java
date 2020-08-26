f1b2f109092c20034b7805b74769f808279852a3
sunrye <srkim8744@korea.kr>
2020-08-27 00:10:06 +0900

    add Test

--- src/exercise/chap05/MyDateTest.java
+++ src/exercise/chap05/MyDateTest.java
@@ -5,6 +5,8 @@ public class MyDateTest {
 	public static void main(String[] args) {
 		MyDate date1 = new MyDate();
 		System.out.println(date1);
+		MyDate date2 = new MyDate(03, 9, 2020);
+		System.out.println(date2);
 	}
 
 }
